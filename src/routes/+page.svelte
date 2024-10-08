9b79ee2f6d6aa7e112d773d2784fbc8c96cbb170
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-10-08 21:08:22 +0200

    align content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1 class="h1">Schützengesellschaft Bleienbach</h1>
-<p>Die Seite ist derzeit im Aufbau, wir freuen uns, sie 2025 präsentieren zu dürfen.</p>
+<div class="flex h-screen items-center justify-center">
+	<div>
+		<h1 class="h1">Schützengesellschaft Bleienbach</h1>
+		<p>Die Seite ist derzeit im Aufbau, wir freuen uns, sie 2025 präsentieren zu dürfen.</p>
+	</div>
+</div>
