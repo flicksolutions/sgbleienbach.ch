11b079a49a25e1faf8705a50095e08b415d7866a
flicksolutions <sebastian@flick.solutions>
2025-09-07 21:20:44 +0200

    update with sponsoring image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
-<div class="flex h-screen items-center justify-center">
+<script lang="ts">
+	import Tafel from '$lib/assets/Tafel.jpg';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center p-4">
 	<div>
 		<h1 class="h1">Schützengesellschaft Bleienbach</h1>
-		<p>Die Seite ist derzeit im Aufbau, wir freuen uns, sie 2025 präsentieren zu dürfen.</p>
+		<p>Die Seite ist derzeit im Aufbau, wir freuen uns, sie bald präsentieren zu dürfen.</p>
 	</div>
+	<img src={Tafel} alt="Sponsoringtafel Neue Scheibenanlage" class="mt-16 rounded-lg" />
 </div>
