4f8330aa443b56b4d5b7eaa72497761c2aec551d
flicksolutions <sebastian@flick.solutions>
2026-01-01 17:16:58 +0100

    setup new v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,2 @@
-<script lang="ts">
-	import Tafel from '$lib/assets/Tafel.jpg';
-</script>
-
-<div class="flex h-screen flex-col items-center justify-center p-4">
-	<div>
-		<h1 class="h1">Schützengesellschaft Bleienbach</h1>
-		<p>Die Seite ist derzeit im Aufbau, wir freuen uns, sie bald präsentieren zu dürfen.</p>
-	</div>
-	<img src={Tafel} alt="Sponsoringtafel Neue Scheibenanlage" class="mt-16 rounded-lg" />
-</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -4,28 +4,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	},
-
+	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
 
