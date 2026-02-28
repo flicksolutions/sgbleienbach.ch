89690bc00c181977ceb5d42d0d44dc5611fad490
flicksolutions <sebastian@flick.solutions>
2026-02-28 19:10:32 +0100

    add fontawesome

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import './layout.css';
+	import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+	import '@fortawesome/fontawesome-free/css/solid.min.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Nav from '$lib/components/Nav.svelte';
 
