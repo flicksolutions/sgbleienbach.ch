0728e8184b0b53b98ee52b1da7314e307c6a3fdf
flicksolutions <sebastian@flick.solutions>
2026-02-28 15:40:20 +0100

    package updates

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,6 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
