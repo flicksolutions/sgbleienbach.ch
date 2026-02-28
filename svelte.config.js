0a990a71a977f59955361ffd675aba29f60edf6b
flicksolutions <sebastian@flick.solutions>
2026-02-28 21:03:07 +0100

    add mdsvex layouts

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
 import { mdsvex } from 'mdsvex';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			layout: {
+				geschichte: join(__dirname, './src/lib/layouts/Geschichte.svelte'),
+				_: join(__dirname, './src/lib/layouts/Default.svelte')
+			}
+		})
+	],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
