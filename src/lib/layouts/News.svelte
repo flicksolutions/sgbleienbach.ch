a9e58f394d2ecd186e7d63c8eaf24c991ccc4716
flicksolutions <sebastian@flick.solutions>
2026-03-25 18:42:23 +0100

    fix colors in news articles

--- src/lib/layouts/News.svelte
+++ src/lib/layouts/News.svelte
@@ -21,11 +21,11 @@
 	<title>{title} – SG Bleienbach</title>
 </svelte:head>
 
-<article class="prose prose-lg mx-auto max-w-3xl dark:prose-invert">
+<article class="mx-auto prose prose-lg max-w-3xl dark:prose-invert">
 	<header class="mb-8">
-		<p class="text-sm font-medium text-surface-500">{formatted}</p>
+		<p class="text-sm font-medium text-secondary-800-200">{formatted}</p>
 		<h1 class="mt-1">{title}</h1>
-		<p class="lead text-surface-600">{abstract}</p>
+		<p class="lead text-secondary-800-200">{abstract}</p>
 	</header>
 	{@render children()}
 </article>
